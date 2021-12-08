be8171b842d203729594a3baa08648d67cc0f06c
ohbumjun <beomjun0638@hanmail.net>
2021-12-08 21:34:07 +0900

    [Feat] Build CShared Ptr Class

--- WinAPI_Project_Puzzle_Game/include/SharedPtr.h
+++ WinAPI_Project_Puzzle_Game/include/SharedPtr.h
@@ -1,26 +1,93 @@
 #pragma once
 
-template<typename T>
+template <typename T>
 class CSharedPtr
 {
-private :
-	T* m_Ptr;
-public :
-	CSharedPtr :
-	m_Ptr(nullptr)
+public:
+	CSharedPtr() :
+		m_Ptr(nullptr)
 	{
 	}
 
-	CSharedPtr(T* ptr)
+	// 너는 복사생성자가 아니야. 사용자 정의 생성자야 ! 
+	CSharedPtr(T* Ptr)
 	{
-		m_Ptr = ptr;
+		m_Ptr = Ptr;
+		
 		if (m_Ptr)
 			m_Ptr->AddRef();
 	}
-public :
-	void operator = (T* Ptr)
+
+	//내가 복사 생성자 
+	CSharedPtr(const CSharedPtr<T>& Ptr)
+	{
+		m_Ptr = Ptr.m_Ptr;
+
+		if (m_Ptr)
+			m_Ptr->AddRef();
+	}
+
+	~CSharedPtr()
 	{
 		if (m_Ptr)
-			m_Ptr->Rel
+			m_Ptr->Release();
+	}
+
+private:
+	T* m_Ptr;
+
+public:
+	void operator = (T* Ptr)
+	{
+		// 기존에 가지고 있는 레퍼런스 객체가 있을 경우 카운트를 1 감소시켜준다.
+		if (m_Ptr)
+			m_Ptr->Release();
+
+		m_Ptr = Ptr;
+
+		// 새로 지정된 레퍼런스 객체가 있을 경우 카운트를 1 증가시켜준다.
+		if (m_Ptr)
+			m_Ptr->AddRef();
+	}
+
+	void operator = (const CSharedPtr<T>& Ptr)
+	{
+		// 기존에 가지고 있는 레퍼런스 객체가 있을 경우 카운트를 1 감소시켜준다.
+		if (m_Ptr)
+			m_Ptr->Release();
+
+		m_Ptr = Ptr.m_Ptr;
+
+		// 새로 지정된 레퍼런스 객체가 있을 경우 카운트를 1 증가시켜준다.
+		if (m_Ptr)
+			m_Ptr->AddRef();
+	}
+
+	bool operator == (T* Ptr)	const
+	{
+		return m_Ptr == Ptr;
+	}
+	bool operator == (const CSharedPtr<T>& Ptr)	const
+	{
+		return m_Ptr == Ptr.m_Ptr;
+	}
+	bool operator != (T* Ptr)	const
+	{
+		return m_Ptr != Ptr;
+	}
+	bool operator != (const CSharedPtr<T>& Ptr)	const
+	{
+		return m_Ptr != Ptr.m_Ptr;
+	}
+
+	// 변환연산자(operator conversion)
+	operator T* ()	const
+	{
+		return m_Ptr;
+	}
+	T* operator -> ()	const
+	{
+		return m_Ptr;
 	}
 };
+
