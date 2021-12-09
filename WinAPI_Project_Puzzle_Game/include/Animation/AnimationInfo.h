0e54018a249d0cb78766140fca9162fccb930760
ohbumjun <beomjun0638@hanmail.net>
2021-12-09 18:53:17 +0900

    [Feat] Build AnimationSequence , AnimationNofity Class

--- WinAPI_Project_Puzzle_Game/include/Animation/AnimationInfo.h
+++ WinAPI_Project_Puzzle_Game/include/Animation/AnimationInfo.h
@@ -1,5 +1,46 @@
 #pragma once
-class CAnimationInfo
+
+#include "../Resource/AnimationSequence.h"
+
+struct  AnimationNotify
 {
+	int Frame;
+	bool Call;
+	std::function<void()> Function;
+
+	AnimationNotify() : 
+		Frame(0),
+		Call(false){}
 };
 
+struct AnimationInfo
+{
+	CSharedPtr<CAnimationSequence> Sequence;
+	int Frame;
+	int FrameTime;
+	int PlayTime;
+	float PlayScale;
+	float m_Time;
+	bool Loop;
+	bool Reverse;
+	std::function<void()> m_EndFunction;
+	std::vector<AnimationNotify*> m_vecNotify;
+
+	AnimationInfo() :
+		Frame(0),
+		FrameTime(0.f),
+		PlayTime(0),
+		PlayScale(1.f),
+		m_Time(0),
+		Loop(false),
+		Reverse(false){}
+
+	~AnimationInfo()
+	{
+		size_t Size = m_vecNotify.size();
+		for (size_t i = 0; i < Size; i++)
+		{
+			SAFE_DELETE(m_vecNotify[i]);
+		}
+	}
+};
--- WinAPI_Project_Puzzle_Game/include/Resource/AnimationSequence.h
+++ WinAPI_Project_Puzzle_Game/include/Resource/AnimationSequence.h
@@ -5,13 +5,39 @@
 class CAnimationSequence :
 	public CRef
 {
+	friend class CResourceManager;
 public:
 	CAnimationSequence();
 	~CAnimationSequence();
 public :
+	ETexture_Type m_TextureType;
 	class CSharedPtr<CTexture> m_Texture;
-	std::vector<AnimationFrameData> m_FrameDatas;
+	std::vector<AnimationFrameData> m_vecFrameData;
 public :
-	void SetTexture();
+	void ClearVecFrameData()
+	{
+		m_vecFrameData.clear();
+	}
+public :
+	CTexture* GetTexture() const
+	{
+		return m_Texture;
+	}
+	ETexture_Type GetTextureType() const
+	{
+		return m_TextureType;
+	}
+	const AnimationFrameData& GetFrameData(int Index) const
+	{
+		return m_vecFrameData[Index];
+	}
+	int GetFrameCount() const
+	{
+		return (int)m_vecFrameData.size();
+	}
+public:
+	bool Init(const std::string& Name, CTexture* Texture);
+	void AddFrameData(const Vector2& StartPos, const Vector2& Size);
+	void AddFrameData(float PosX, float PosY, float SizeX, float SizeY);
 };
 
